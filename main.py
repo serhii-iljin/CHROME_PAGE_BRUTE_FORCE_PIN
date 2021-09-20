6f5fc6d58b6cd519e86f53884c2844f4545dc0c4
Serhii Iljin <89367614+serhii-iljin@users.noreply.github.com>
2021-09-21 00:10:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,25 +13,30 @@ if __name__ == '__main__':
     openchest.click()
     time.sleep(2)
     passwordfield = browser.find_element(By.XPATH, "//*[@id=\"slide-password-wrapper-ca9b7634-e002-40f2-b6c3-cca0ad285c3d\"]/div/div/div/form/div/div[1]/input")
+    time.sleep(2)                      
+    #Code before this line describes path to the password field
+    #Code below describes password brute forcing 
     time.sleep(2)
-    passwordfield.send_keys('00000')
-    passwordfield.send_keys('\n')
-    time.sleep(10)
-    passwordfield.clear()
-    for i in range(34900, 100000):
-        t = i
-        c = 5
+    for i in range(0, 100000):
+        t = i #Temp. variable to count number of digits needed to be cover with zeroes
+        c = 5 #Number of zeroes set to 5 from the scrath
         while (t != 0):
-            t //= 10
+            t //= 10    #Counting digits needed to be covered with zeroes
             c -= 1
+        s = ""
         for j in range(0, c):
-            passwordfield.send_keys(0)
-        passwordfield.send_keys(i)
-        passwordfield.send_keys('\n')
-        time.sleep(0.5)
-        passwordfield.clear()
-        passwordfield.clear()
-        passwordfield.clear()
-        passwordfield.clear()
-        passwordfield.clear()
-        passwordfield.clear()
+            s += str(0).       #Covering needed digits with zeroes 
+        s += str(i)+'\n'      
+        passwordfield.send_keys(s)    #This string method of sending keys fasters brute forcing 
+        for j in range(1, 10):        #To make sure that the field is cleared.
+            passwordfield.clear()
+
+
+
+
+
+
+
+
+
+
